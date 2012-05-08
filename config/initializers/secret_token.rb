682ca90fc9bb58c47be4a653d5cc311a1d33169a
B Kleinen <kleinen@infrastructure.de>
2012-05-08 18:19:51 +0200

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studentmemory::Application.config.secret_token = 'a3588f10e0e7932392e330a904e5e8d7286abbbeb8a867c3cf3382bd75c7a4c571d2f5668e8914e22358d0299c1067fb64b81b1f18b869e5003360632954b0ff'
