7b6a28a9ca125c56774dc69998c3c6dcc12e8469
B Kleinen <kleinen@infrastructure.de>
2012-05-09 00:13:53 +0200

    removed some empty lines

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,3 @@
-class Person < ActiveRecord::Base
+class Person < ActiveRecord::Base  
   has_and_belongs_to_many :courses
 end
--- db/migrate/20120508205743_rename_table_students_to_persons.rb
+++ db/migrate/20120508205743_rename_table_students_to_persons.rb
@@ -2,5 +2,4 @@ class RenameTableStudentsToPersons < ActiveRecord::Migration
   def change
     rename_table :students, :people
   end
-
 end
